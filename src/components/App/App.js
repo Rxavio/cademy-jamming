b0a0418e1266b895d7d4f442bc5ef872f23656d4
xavioo <rucahoxavi@gmail.com>
2022-01-19 04:36:17 +0200

    setup and start jamming

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      </div>
+    );
+  }
+}
+export default App;
